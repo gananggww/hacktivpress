9fad03eb15b661622dcf649633db1e28904ec39b
gananggww <gananggww@gmail.com>
2017-09-14 14:45:35 +0700

    tambah client

---
+++ client/hacktivpress/src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ui container">
+    <div class="ui segment">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
